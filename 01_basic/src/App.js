a4ec0a96d31f1563300620e07632695adf559d5d
suhana <suhanakaura01@gmail.com>
2024-04-12 20:29:18 +0530

    changes

---
+++ 01_basic/src/App.js
@@ -0,0 +1,9 @@
+
+function App() {
+  return (
+    // jsx
+    <h1>hello world</h1>
+  );
+}
+
+export default App;
---
+++ 01_viteReact/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+    <h1>chai aur code using vite</h1>
+  )
+}
+
+export default App
